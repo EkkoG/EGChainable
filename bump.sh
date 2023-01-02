c95cede5e68d1bc7365edf78d5213fbaa8fa229e
EkkoG <beijiu572@gmail.com>
2023-01-02 22:49:02 +0800

    Refine bump script

--- bump.sh
+++ bump.sh
@@ -4,6 +4,8 @@ VERSION=`grep -m 1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" EGChainable.podspec`
 git add .
 git commit -m "Bump version to $VERSION"
 git push origin main
-git tag $VERSION
-git push --tags
+
+gh release create $VERSION --title $VERSION --notes "Release $VERSION" --target main
+
 pod trunk push EGChainable.podspec --allow-warnings
+
