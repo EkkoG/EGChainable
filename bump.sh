0fc1aaaabf26385e2bf601564f6253a2569dc8ad
Ekko <beijiu572@gmail.com>
2023-01-03 11:30:32 +0800

    Make bump script more generic

--- bump.sh
+++ bump.sh
@@ -1,14 +1,15 @@
 # get versiom from podspec
 
 # minor version bump
-OLD_VERSION=`grep -m 1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" EGChainable.podspec`
+OLD_VERSION=`grep -m 1 -Eo "[0-9]+\.[0-9]+\.[0-9]+" $1.podspec`
 # add 1 to minor version
 VERSION=`echo $OLD_VERSION | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g'`
-sed -i '' "s/$OLD_VERSION/$VERSION/g" EGChainable.podspec
-git add .
-git commit -m "Bump version to $VERSION"
-git push origin main
+sed -i '' "s/$OLD_VERSION/$VERSION/g" $1.podspec
+cat $1.podspec
+# git add .
+# git commit -m "Bump version to $VERSION"
+# git push origin main
 
-gh release create $VERSION --title $VERSION --notes "Release $VERSION" --target main --generate-notes
+# gh release create $VERSION --title $VERSION --notes "Release $VERSION" --target main --generate-notes
 
-pod trunk push EGChainable.podspec --allow-warnings
+# pod trunk push $1.podspec --allow-warnings
